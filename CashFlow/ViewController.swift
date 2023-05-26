634c66224783b3cafa01710350a397788faa7b45
magomedkadiev <sultan.magomedkadiev@gmail.com>
2023-05-26 21:46:40 +0300

    Initial commit

--- CashFlow/ViewController.swift
+++ CashFlow/ViewController.swift
@@ -1,19 +1,10 @@
-//
-//  ViewController.swift
-//  CashFlow
-//
-//  Created by Султанхан on 26.05.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
 }
 
